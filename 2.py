e0ae8f065a2c151c27c4a8ced6997dd64e57dd00
DugarovBator <bdugarov332@gmail.com>
2025-06-24 09:52:15 +0800

    фывфывыфв

--- 2.py
+++ 2.py
@@ -1,1 +1,16 @@
-print(1)
+from pygame import *
+
+class Object(sprite.Sprite):
+    # конструктор класса
+    def __init__(self, player_image, player_x, player_y, player_speed):
+        super().__init__()
+        # каждый спрайт должен хранить свойство image - изображение
+        self.image = transform.scale(image.load(player_image), (55, 55))
+        self.speed = player_speed
+        # каждый спрайт должен хранить свойство rect - прямоугольник, в который он вписан
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
